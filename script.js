df83f338c97a596ead833d7319cc75407fdf670f
Your Namemh <mh.cse91@gmail.com>
2026-02-24 22:16:04 +0600

    tab filtering function added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let rejectedCount = document.getElementById('rejectedCount');
 
 // total count dashboard section
 const allJobCardsSection = document.getElementById('allJobCards')
+const mainContainer = document.querySelector('main')
 
 function calculateTotalCount() {
   totalCount.innerText = allJobCardsSection.children.length;
@@ -17,3 +18,23 @@ function calculateTotalCount() {
 calculateTotalCount()
 
 
+// button toggole structure
+
+const allFilterTab = document.getElementById('allTab');
+const interviewFilterTab = document.getElementById('interviewTab');
+const rejectedFilterTab = document.getElementById('rejectedTab');
+
+function changeTab(id) {
+  allFilterTab.classList.remove('bg-blue-600', 'text-white')
+  interviewFilterTab.classList.remove('bg-blue-600', 'text-white')
+  rejectedFilterTab.classList.remove('bg-blue-600', 'text-white')
+
+  allFilterTab.classList.add('bg-gray-200', 'text-black')
+  interviewFilterTab.classList.add('bg-gray-200', 'text-black')
+  rejectedFilterTab.classList.add('bg-gray-200', 'text-black')
+
+  const selectedTab = document.getElementById(id)
+
+  selectedTab.classList.remove('bg-gray-200', 'text-black')
+  selectedTab.classList.add('bg-blue-600', 'text-white')
+}
