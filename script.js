a62999914522530c2754fe0dacc0d86c479ca452
Your Namemh <mh.cse91@gmail.com>
2026-02-27 05:42:45 +0600

    event delegationimplement  for interview, rejected, and delete action

--- script.js
+++ script.js
@@ -92,3 +92,85 @@ function changeTab(id) {
 
   calculateTotalCount()
 }
+
+
+// Event delegations
+
+mainContainer.addEventListener('click', function (event) {
+
+  if (event.target.classList.contains('interview-btn')) {
+
+    const nparentNode = event.target.closest('.bg-white');
+
+    const companyName = nparentNode.querySelector('.company-name')?.innerText;
+    const jobPosition = nparentNode.querySelector('.job-position')?.innerText;
+    const salaryInfo = nparentNode.querySelector('.salary-info')?.innerText;
+    const jobInfo = nparentNode.querySelector('.job-info')?.innerText;
+
+    const statusSpan = nparentNode.querySelector('.apply-status');
+
+    if (statusSpan) {
+      statusSpan.innerText = "INTERVIEW";
+      statusSpan.classList.remove('bg-gray-300', 'text-gray-900');
+      statusSpan.classList.remove('bg-red-200', 'text-red-900');
+      statusSpan.classList.add('bg-green-200', 'text-green-900');
+    }
+
+    rejectedList = rejectedList.filter(
+      item => item.companyName !== companyName
+    );
+
+    const listExist = interviewList.find(
+      item => item.companyName === companyName
+    )
+
+    if (!listExist) {
+      interviewList.push({ companyName, jobPosition, salaryInfo, jobInfo })
+    }
+
+    if (currentTab === "interview") renderInterview()
+    if (currentTab === "rejected") renderRejected()
+
+    calculateTotalCount()
+  }
+
+  if (event.target.classList.contains('rejected-btn')) {
+
+    const nparentNode = event.target.closest('.bg-white');
+
+    const companyName = nparentNode.querySelector('.company-name')?.innerText;
+    const jobPosition = nparentNode.querySelector('.job-position')?.innerText;
+    const salaryInfo = nparentNode.querySelector('.salary-info')?.innerText;
+    const jobInfo = nparentNode.querySelector('.job-info')?.innerText;
+
+    const statusSpan = nparentNode.querySelector('.apply-status');
+
+    if (statusSpan) {
+      statusSpan.innerText = "REJECTED";
+      statusSpan.classList.remove('bg-gray-300', 'text-gray-900');
+      statusSpan.classList.remove('bg-green-200', 'text-green-900');
+      statusSpan.classList.add('bg-red-200', 'text-red-900');
+    }
+
+    interviewList = interviewList.filter(
+      item => item.companyName !== companyName
+    );
+
+    const listExist = rejectedList.find(
+      item => item.companyName === companyName
+    )
+
+    if (!listExist) {
+      rejectedList.push({ companyName, jobPosition, salaryInfo, jobInfo })
+    }
+
+    if (currentTab === "interview") renderInterview()
+    if (currentTab === "rejected") renderRejected()
+
+    calculateTotalCount()
+  }
+
+
+
+});
+
