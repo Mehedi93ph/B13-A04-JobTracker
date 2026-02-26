34a576fc9d664fa306a519956b34fbd1d9763e23
Your Namemh <mh.cse91@gmail.com>
2026-02-27 05:00:49 +0600

    dashboard counter updating, toggole style setup

--- script.js
+++ script.js
@@ -47,3 +47,48 @@ function calculateTotalCount() {
 }
 
 calculateTotalCount();
+
+
+// Tab toggole
+
+const allFilterTab = document.getElementById('allTab');
+const interviewFilterTab = document.getElementById('interviewTab');
+const rejectedFilterTab = document.getElementById('rejectedTab');
+
+function changeTab(id) {
+
+  allFilterTab.classList.remove('bg-blue-600', 'text-white')
+  interviewFilterTab.classList.remove('bg-blue-600', 'text-white')
+  rejectedFilterTab.classList.remove('bg-blue-600', 'text-white')
+
+  allFilterTab.classList.add('bg-gray-200', 'text-black')
+  interviewFilterTab.classList.add('bg-gray-200', 'text-black')
+  rejectedFilterTab.classList.add('bg-gray-200', 'text-black')
+
+  const selectedTab = document.getElementById(id)
+
+  selectedTab.classList.remove('bg-gray-200', 'text-black')
+  selectedTab.classList.add('bg-blue-600', 'text-white')
+
+  filteredCard.innerHTML = ""
+
+  if (id === "allTab") {
+    currentTab = "all"
+    allJobCardsSection.style.display = "block"
+    emptyState.classList.add('hidden')
+  }
+
+  if (id === "interviewTab") {
+    currentTab = "interview"
+    allJobCardsSection.style.display = "none"
+    renderInterview()
+  }
+
+  if (id === "rejectedTab") {
+    currentTab = "rejected"
+    allJobCardsSection.style.display = "none"
+    renderRejected()
+  }
+
+  calculateTotalCount()
+}
